ea2c62041d0232920e0ad9834f31d4be474fbfdd
katerynafomina <kf30766@gmail.com>
2024-05-13 00:11:06 +0300

    add auth

---
+++ src/lib/supabase.ts
@@ -0,0 +1,16 @@
+import { AppState } from 'react-native'
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL='https://kneqncbpyahhxkftsosp.supabase.co'
+const SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZXFuY2JweWFoaHhrZnRzb3NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MTk0MjAsImV4cCI6MjAzMTA5NTQyMH0.k2Wc2e0mYElOJBlUUrUyy8BrWe-AO-x_258w1CYvO8Q'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
